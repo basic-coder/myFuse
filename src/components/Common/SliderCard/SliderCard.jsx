99209f6e092c660b3408637f058ea6ffdbb6ae31
sakpalbhavesh <bhavesh.sakpal@teknopoint.in>
2024-03-19 03:37:49 +0530

    hero banner added

---
+++ src/components/Common/SliderCard/SliderCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { IoLocationSharp } from "react-icons/io5";
+import './sliderCard.css'
+
+const SliderCard = ({data}) => {
+  return (
+    <div className='slider__card'>
+        <div className="slider__card_img"><img src={data.logo} alt="logo" /></div>
+        <div className="slider__card_content">
+            <h3 className="slider__card_company">{data.company}</h3>
+
+            <div className="slider__card_field_location">
+                <span>{data.field}</span> <span><IoLocationSharp />{data.location}</span>
+            </div>
+            <div className="slider__card_role">
+               Job Role :  {data.role}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SliderCard
