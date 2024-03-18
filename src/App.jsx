c58ac7dee8e19b6d6dc749e7e00b1542d7235fba
sakpalbhavesh <bhavesh.sakpal@teknopoint.in>
2024-03-19 02:20:41 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import { useState } from 'react'
 
 function App() {
+
+  const [navActive, setNavActive] = useState()
 
   return (
     <BrowserRouter>
       <div className='main'>
         
+        <Navbar  setNavActive={setNavActive} navActive={navActive}/>
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+        <div className={`overlay ${navActive ? 'active': ''}`}></div>
 
       </div>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './navbar.css'
+import { Link } from 'react-router-dom'
+import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { useState } from 'react';
+import { IoMdClose } from "react-icons/io";
 
-const Navbar = () => {
+const Navbar = ({setNavActive,navActive}) => {
+
+  
+  const [linkActive, setLinkActive] = useState(0)
+
+  const navArray = [
+    { id: 0, label: "Home", link: "/" },
+    { id: 1, label: "Sign in", link: "/signin" },
+    { id: 2, label: "About Us", link: "/aboutus" },
+    { id: 3, label: "Blogs", link: "/blogs" },
+    { id: 4, label: "Contact Us", link: "/contactUs" }
+  ]
+
   return (
-    <div>Navbar</div>
+    <>
+    <div className='navbar '>
+      <div className="container">
+      <h1 className='nav__logo'>MyFuse<span>.</span>in</h1>
+
+      <span className="nav__icon" onClick={() => setNavActive(!navActive)}><HiMiniBars3CenterLeft /></span>
+      <ul className={`nav__links ${navActive ? 'navActive' : ''} `}>
+        <span className="nav__icon close" onClick={() => setNavActive(!navActive)}><IoMdClose /></span>
+
+        {navArray.map((data)=> <li key={data.id}><Link className={`nav__link ${data.id == linkActive ? 'active' : ''}`}  to={data.link} onClick={() => setLinkActive(data.id)}>{data.label}</Link></li>)}
+      </ul>
+      </div>
+    </div>
+    </>
   )
 }
 
