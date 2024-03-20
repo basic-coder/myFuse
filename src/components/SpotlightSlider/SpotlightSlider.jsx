219327469e9c89e70b795d3e4aacbbaf10bd118b
sakpalbhavesh <bhavesh.sakpal@teknopoint.in>
2024-03-20 15:41:43 +0530

    wip

--- src/components/SpotlightSlider/SpotlightSlider.jsx
+++ src/components/SpotlightSlider/SpotlightSlider.jsx
@@ -1,45 +1,23 @@
 import React from 'react'
 import './spotlightSlider.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { Autoplay } from 'swiper/modules';
 
 const SpotlightSlider = ({ items }) => {
+
   return (
     <div className='spotlightSlider container'>
 
-
-      <Swiper
-        // install Swiper modules
-        className='spotlightSlider__container'
-        spaceBetween={50}
-        loop={true}
-        slidesPerView={3}
-        grabCursor={true}
-        keyboard={{
-          enabled: true,
-        }}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: true,
-        }}
-        breakpoints={{
-          1180: {
-            slidesPerView: 5,
-          },
-          767: {
-            slidesPerView: 4,
-          },
-        }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        modules={[Autoplay]}
-        onSlideChange={() => console.log('slide change')}
-      >
-        {
-          items?.map((item, index) => (<SwiperSlide> <div className="spotlightSlider__img"> <img key={index} src={item} alt="" /> </div></SwiperSlide>))
-        }
-      </Swiper>
+      <div className="logos">
+        <div className="logos-slide">
+          {
+            items?.map((item, index) => (<img key={index} src={item} alt="" />))
+          }
+          {/* Duplicate the images to create a seamless loop */}
+          {
+            items?.map((item, index) => (<img key={`duplicate-${index}`} src={item} alt="" />))
+          }
+        </div>
+      </div>
     </div>
   )
 }
